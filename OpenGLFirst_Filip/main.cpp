27713c6a710db28a24ad40167a989d57bc6364d6
kojonek2 <kojonek2@wp.pl>
2017-08-21 14:10:41 +0200

    added unbinding

--- OpenGLFirst_Filip/main.cpp
+++ OpenGLFirst_Filip/main.cpp
@@ -115,6 +115,9 @@ int main()
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 
 	while (!glfwWindowShouldClose(window))
